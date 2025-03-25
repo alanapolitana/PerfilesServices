1e699092ecd152858ad07adb3cedbef33bd1090d
MarcoV <marcovirinni@gmail.com>
2025-03-25 16:08:07 -0300

    MVP basico, correcion DB

--- Perfiles/settings.py
+++ Perfiles/settings.py
@@ -25,19 +25,17 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Configuración de la base de datos
 DATABASES = {
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
-
-# Si tienes configuraciones adicionales para otras variables:
+# Agrega explícitamente el puerto si es necesario
 DATABASES['default'].update({
-    'USER': os.getenv('USER', 'default_user'),
-    'PASSWORD': os.getenv('PASSWORD', 'default_password'),
-    'HOST': os.getenv('HOST', 'localhost'),
-    'PORT': os.getenv('PORT', 5432),
+    'PORT': os.getenv('PORT', 5432)  # Si no existe PORT, usará el valor 5432
 })
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
