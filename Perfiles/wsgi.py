6fec14e56bcb24aac82b5ff2cef23e5963d0fde0
MarcoV <marcovirinni@gmail.com>
2025-03-26 16:56:33 -0300

    user

--- Perfiles/wsgi.py
+++ Perfiles/wsgi.py
@@ -15,11 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Perfiles.settings')
 
 application = get_wsgi_application()
 
-
-
-
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "admin123")
